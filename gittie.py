80a2ada6424f9c9b7ca46ca4d24735ef2358d3ab
aneta.kruc <aneta.kruc@gmail.com>
2020-03-22 10:27:15 +0100

    define add func

--- gittie.py
+++ gittie.py
@@ -1,2 +1,11 @@
 def introduce():
     print(f"Hello, I'm Gittie.")
+
+
+def add():
+    my_list = []
+    added_item = input("please add sth")
+    my_list.append(added_item)
+
+    print(my_list)
+    
